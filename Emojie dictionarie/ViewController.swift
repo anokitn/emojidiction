d25979b78d645754bcb754ae3291fcc87eb42cef
Anokitn <sevzero@yandex.com>
2019-01-11 14:53:58 +0100

    cf g t gtvgtvgtv

--- Emojie dictionarie/ViewController.swift
+++ Emojie dictionarie/ViewController.swift
@@ -8,13 +8,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tabel1: UITableView!
+    
+    var emoj = ["😅","😎","🤬","😷","🤕","😺","🐙","🐋"]
 
+   //========== = == = = = = = = = = = = = =
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        tabel1.dataSource = self
+        tabel1.delegate = self
+        
     }
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int { return emoj.count
+    
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {         print(indexPath.row)
+        let cell = UITableViewCell()
+        cell.textLabel?.text = emoj[indexPath.row]
+        return cell
+        
+    }
 
 }
 
